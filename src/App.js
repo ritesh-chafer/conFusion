85f8ec8ba388800fd80566a1e6f72c5fb2d4bebb
ritesh-chafer <riteshprasadsingh8@gmail.com>
2020-04-30 04:08:12 +0530

    components part C2

--- src/App.js
+++ src/App.js
@@ -1,23 +1,31 @@
-import React from 'react';
-import logo from './logo.svg';
-import { Navbar, NavbarBrand } from 'reactstrap';
-import Menu from './components/MenuComponents'
-import './App.css';
+import React, {Component} from "react";
+import logo from "./logo.svg";
+import { Navbar, NavbarBrand } from "reactstrap";
+import Menu from "./components/MenuComponents";
+import "./App.css";
+import { DISHES } from "./shared/dishes";
+import { render } from "@testing-library/react";
 
-function App() {
-  return (
-    <div>
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      dishes : DISHES
+    };
+  }
+  render() {
+    return (
+      <div>
+        <Navbar dark color="primary" expand="lg">
+          <div className="container">
+            <NavbarBrand href="/">Ristorante Con Fusion</NavbarBrand>
+          </div>
+        </Navbar>
 
-      <Navbar dark color="primary" expand="lg">
-        <div className="container">
-          <NavbarBrand href="/">Ristorante Con Fusion</NavbarBrand>
-        </div>
-      </Navbar>
-
-      <Menu />
-
-    </div>
-  );
+        <Menu dishes={this.state.dishes}/>
+      </div>
+    );
+  }
 }
 
 export default App;
