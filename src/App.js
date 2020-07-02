3bbbfbfbfce77618940229f35be44723beb2575e
ritesh-chafer <riteshprasadsingh8@gmail.com>
2020-07-02 18:29:51 +0530

    Minor Changes

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import "./App.css";
 import { render } from "@testing-library/react";
 import { BrowserRouter } from "react-router-dom";
 import { Provider } from "react-redux";
-import { ConfugureStore } from "./redux/configureStore";
+import { ConfigureStore } from "./redux/configureStore";
 
 const store = ConfugureStore();
 
