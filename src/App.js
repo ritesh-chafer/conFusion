b47ed910685555c040ce868c26fe0d45b1374415
ritesh-chafer <riteshprasadsingh8@gmail.com>
2020-07-02 18:22:57 +0530

    Importing necessary items in app.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import React, { Component } from "react";
-
 import { Navbar, NavbarBrand } from "reactstrap";
 import Main from "./components/MainComponent";
 import "./App.css";
-
 import { render } from "@testing-library/react";
 import { BrowserRouter } from "react-router-dom";
+import { Provider } from "react-redux";
+import { ConfugureStore } from "./redux/configureStore";
 
 class App extends Component {
   render() {
