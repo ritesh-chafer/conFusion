97d87dcd69c5089dd2ee5b3ba84d4abb555fa0e6
ritesh-chafer <riteshprasadsingh8@gmail.com>
2020-07-02 18:30:23 +0530

    Minor Changes

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ import { BrowserRouter } from "react-router-dom";
 import { Provider } from "react-redux";
 import { ConfigureStore } from "./redux/configureStore";
 
-const store = ConfugureStore();
+const store = ConfigureStore();
 
 class App extends Component {
   render() {
