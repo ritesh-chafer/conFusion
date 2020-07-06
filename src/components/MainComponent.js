551cc7fa8b125f8772f6b47502112c6426efecdc
ritesh-chafer <riteshprasadsingh8@gmail.com>
2020-07-07 00:10:50 +0530

    updating MainComponent.js to dispatch the comments taken by payload

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -10,6 +10,9 @@ import About from "./AboutComponent";
 import { Switch, Route, Redirect, withRouter } from "react-router-dom";
 import { connect } from 'react-redux';
 
+import { addComment } from '../redux/ActionCreators';
+
+
 const mapStateToProps = state  => {
     return{
       dishes: state.dishes,
@@ -18,6 +21,10 @@ const mapStateToProps = state  => {
       leaders: state.leaders
     }
 }
+
+const mapDispatchToProps = (dispatch) => ({
+    addComment:(dishId, rating, author, comment) => dispatch(addComment(dishId, rating, author, comment))
+});
 
 class Main extends Component {
   constructor(props) {
@@ -47,6 +54,7 @@ class Main extends Component {
           comments={this.props.comments.filter(
             (comment) => comment.dishId === parseInt(match.params.dishId, 10)
           )}
+          addComment = {this.props.addComment}
         />
       );
     };
@@ -75,5 +83,5 @@ class Main extends Component {
     );
   }
 }
-export default withRouter(connect(mapStateToProps)(Main));
+export default withRouter(connect(mapStateToProps, mapDispatchToProps)(Main));
 
