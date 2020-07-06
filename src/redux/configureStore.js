89a27238ab4f27287decd3ff332225713612b210
ritesh-chafer <riteshprasadsingh8@gmail.com>
2020-07-06 22:31:01 +0530

    Setting configureStore.js

--- src/redux/configureStore.js
+++ src/redux/configureStore.js
@@ -1,5 +1,8 @@
-import { createStore, bindActionCreators } from 'redux';
-import {Reducer, initialState } from './reducer';
+import { createStore, combineReducers } from 'redux';
+import { Dishes } from './dishes';
+import { Comments } from './comments';
+import { Promotions } from './promotions';
+import { Leaders } from './leaders';
 
 export const ConfigureStore = () => {
     const store = createStore(Reducer,initialState);
