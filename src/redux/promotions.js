963374eb734cacbee3cb6143244f06a86cdd6134
ritesh-chafer <riteshprasadsingh8@gmail.com>
2020-07-06 22:25:28 +0530

    Splitting reducers

---
+++ src/redux/promotions.js
@@ -0,0 +1,8 @@
+import { PROMOTIONS } from "../shared/promotion";
+
+export const promotion = (state = PROMOTIONS, action) => {
+    switch(action.type){
+        default:
+            return state;
+    }
+}
