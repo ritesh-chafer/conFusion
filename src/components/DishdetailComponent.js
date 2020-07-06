d7241c912f879f0e4fbfc2457953d176b00d5fcf
ritesh-chafer <riteshprasadsingh8@gmail.com>
2020-07-07 00:21:54 +0530

    Adding comments

--- src/components/DishdetailComponent.js
+++ src/components/DishdetailComponent.js
@@ -31,7 +31,7 @@ function RenderDish({ dish }) {
     </div>
   );
 }
-function RenderComments({ comments }) {
+function RenderComments({ comments, addComment, dishId}) {
   if (comments != null)
     return (
       <div className="col-12 col-md-5 m-1">
@@ -53,7 +53,8 @@ function RenderComments({ comments }) {
             );
           })}
         </ul>
-        <CommentForm />
+        <CommentForm dishId={dishId} addComment ={addComment}
+        />
       </div>
     );
   else return <div></div>;
@@ -77,7 +78,10 @@ const DishDetail = (props) => {
         </div>
         <div className="row">
           <RenderDish dish={props.dish} />
-          <RenderComments comments={props.comments} />
+          <RenderComments comments={props.comments} 
+          addComment={props.addComment} 
+          dishId = {props.dish.id}
+          />
         </div>
       </div>
     );
@@ -110,9 +114,8 @@ export class CommentForm extends Component {
 
   handleSubmit(values) {
     this.toggleModal();
-
-    console.log("comment:", values);
-    alert("comment:" + JSON.stringify(values));
+    this.props.addComment(this.props.dishId, values.rating, values.author, values.comment);
+    
   }
 
   render() {
