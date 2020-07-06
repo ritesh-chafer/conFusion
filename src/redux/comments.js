693b77b88a069dbd60bbcb80147aabd303079664
ritesh-chafer <riteshprasadsingh8@gmail.com>
2020-07-07 00:01:58 +0530

    updating commet.js

--- src/redux/comments.js
+++ src/redux/comments.js
@@ -1,8 +1,14 @@
 import { COMMENTS } from "../shared/comment";
+import { actionTypes } from "react-redux-form";
 
 
 export const Comments = (state = COMMENTS, action) => {
     switch(action.type){
+        case actionTypes.ADD_COMMENT:
+            var comment = action.payload;
+            comment.id = state.length;
+            comment.date = new Date().toISOString();
+            return state.concat(comment);
         default:
             return state;
     }
