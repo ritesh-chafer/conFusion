10804a4762f81f2e208e30123c005e6553832933
ritesh-chafer <riteshprasadsingh8@gmail.com>
2020-07-08 05:18:12 +0530

    adding

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -41,7 +41,9 @@ class Main extends Component {
     const HomePage = () => {
       return (
         <Home
-          dish={this.props.dishes.filter((dish) => dish.featured)[0]}
+          dish={this.props.dishes.dishes.filter((dish) => dish.featured)[0]}
+          dishesLoading = {this.props.dishes.isLoading}
+          dishesErrMess = {this.props.dishes.errMess}
           promotion={this.props.promotions.filter((promo) => promo.featured)[0]}
           leader={this.props.leaders.filter((lead) => lead.featured)[0]}
         />
@@ -52,10 +54,12 @@ class Main extends Component {
       return (
         <Dishdetail
           dish={
-            this.props.dishes.filter(
+            this.props.dishes.dishes.filter(
               (dish) => dish.id === parseInt(match.params.dishId, 10)
             )[0]
           }
+          isLoading = {this.props.dishes.isLoading}
+          errMess = {this.props.dishes.errMess}
           comments={this.props.comments.filter(
             (comment) => comment.dishId === parseInt(match.params.dishId, 10)
           )}
