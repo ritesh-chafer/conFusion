d683c26160c7ce5d0bcb4ba7e564386c9a74f6cf
ritesh-chafer <riteshprasadsingh8@gmail.com>
2020-07-08 05:40:53 +0530

    loading added

--- src/components/LoadingComponent.js
+++ src/components/LoadingComponent.js
@@ -3,7 +3,7 @@ import React from 'react';
 export const Loading = () => {
 return(
     <div className="col-12">
-        <span className="fa fa-spinner fa-pulse fa-3x fa-fw text-primary"></span>
+        <span className="fa fa-spinner fa-pulse fa-2x fa-fw text-primary"></span>
         <p>Loading . . .</p>
     </div>
 );
