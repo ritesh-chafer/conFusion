2b5737263e074b33ed107f9ea19a692c1bf25adb
ritesh-chafer <riteshprasadsingh8@gmail.com>
2020-07-08 05:45:28 +0530

    making use of redux in form

--- src/redux/configureStore.js
+++ src/redux/configureStore.js
@@ -6,6 +6,8 @@ import { Leaders } from './leaders';
 import thunk from 'redux-thunk';
 import logger from 'redux-logger';
 
+
+
 export const ConfigureStore = () => {
     const store = createStore(
         combineReducers({
