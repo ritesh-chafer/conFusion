bb5925ebc19a438e7f348cecc5c3d5cd6eda5d9b
ritesh-chafer <riteshprasadsingh8@gmail.com>
2020-07-10 00:21:46 +0530

    editing comments.js

--- src/redux/ActionCreators.js
+++ src/redux/ActionCreators.js
@@ -1,5 +1,6 @@
 import * as ActionTypes from './ActionTypes';
 import { DISHES } from "../shared/dish";
+
 import { baseUrl } from "../shared/baseUrl";
 
 export const addComment = (dishId, rating, author, comment) => ({
