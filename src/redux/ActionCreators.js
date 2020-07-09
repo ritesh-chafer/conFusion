5b78dbdcb1d41cab9945a9ba5ebd24df5f7d5b7c
ritesh-chafer <riteshprasadsingh8@gmail.com>
2020-07-09 21:07:25 +0530

    Adding addCommnets to add comments with baseURl

--- src/redux/ActionCreators.js
+++ src/redux/ActionCreators.js
@@ -46,3 +46,8 @@ export const commentsFailed = (errmess) => ({
     type: ActionTypes.COMMENTS_FAILED,
     payload: errmess
 });
+
+export const addComments = (comments) => ({
+    type: ActionTypes.ADD_COMMENTS,
+    payload: comments
+});
