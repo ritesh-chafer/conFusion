7c6c6c3a33ead8185435811eeb39cba5864ccf87
ritesh-chafer <riteshprasadsingh8@gmail.com>
2020-07-10 00:28:12 +0530

    :)

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -49,13 +49,16 @@ class Main extends Component {
   render() {
     const HomePage = () => {
       return (
-        <Home
-          dish={this.props.dishes.dishes.filter((dish) => dish.featured)[0]}
-          dishesLoading = {this.props.dishes.isLoading}
-          dishesErrMess = {this.props.dishes.errMess}
-          promotion={this.props.promotions.filter((promo) => promo.featured)[0]}
-          leader={this.props.leaders.filter((lead) => lead.featured)[0]}
+        <Home 
+        dish={this.props.dishes.dishes.filter((dish) => dish.featured)[0]}
+        dishesLoading={this.props.dishes.isLoading}
+        dishErrMess={this.props.dishes.errMess}
+        promotion={this.props.promotions.promotions.filter((promo) => promo.featured)[0]}
+        promoLoading={this.props.promotions.isLoading}
+        promoErrMess={this.props.promotions.errMess}
+        leader={this.props.leaders.filter((leader) => leader.featured)[0]}
         />
+    
       );
     };
 
